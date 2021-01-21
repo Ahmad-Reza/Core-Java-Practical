ba2a562cf32409103480b35de46b1f38fa4aa017
Ahmad-Reza <ahmadrezambit@gmail.com>
2021-01-21 11:55:24 +0530

    casting example

---
+++ CastingExam.java
@@ -0,0 +1,22 @@
+/**
+ * Casting :
+ * conversion one data type to another data type
+ * 
+ */
+
+public class CastingExam {
+
+    public static void main(String[] args) {
+        
+        // 1. implicit casting "done by compiler"
+            int value = 100;
+            long v = value;
+            System.out.println("implicit exam:"+v);
+
+        // 1. explicit casting "done by code(which is write by us)"
+            long value1 = 123445;
+            int v1 = (int)value1;
+            System.out.println("explicit exam:"+v1);
+    }
+    
+}
